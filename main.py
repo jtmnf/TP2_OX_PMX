7890ee5bd72ebcc54255fd25c2d03bf8ba95e5dd
jtmnf <jtmnf@hotmail.com>
2016-05-02 19:04:45 +0100

    Clean up;

--- main.py
+++ main.py
@@ -3,57 +3,58 @@ from Operators.LocalSearchOperators import *
 from Problem.ProblemRepresentation import *
 
 if __name__ == '__main__':
-	Recombinations = [{'operator':IntegerOperators.cross_pmx,'display':'PMX'},{'operator':IntegerOperators.cross_order,'display':'OX'}]
+    Recombinations = [{'operator': IntegerOperators.cross_pmx, 'display': 'PMX'},
+                      {'operator': IntegerOperators.cross_order, 'display': 'OX'}]
 
-	Problems = []
-	
-	'''
-	Problem = {}
-	Problem['name'] = 'jb'
-	Problem['config'] = {'name':Problem['name'],'dimension':50}
-	Problem['generate_pop'] = ProblemRepresentation.generateJBPopulation
-	Problem['fitness'] = ProblemRepresentation.standardFitnessJB
-	Problems.append(Problem)
-	'''
-	
-	Problem = {}
-	Problem['name'] = 'tsp'
-	TSPCoord = ProblemRepresentation.readTSPCoord('Data/TSP/wi29.tsp')
-	TSPDetails = ProblemRepresentation.parseTSPCoord(TSPCoord)
-	Problem['config'] = {'name':Problem['name'],'TSPDetails':TSPDetails,'cromo_size':len(TSPDetails),'optimization':Algorithm.Minimization}
-	Problem['generate_pop'] = ProblemRepresentation.generateTSPPopulation
-	Problem['fitness'] = ProblemRepresentation.standardFitnessTSP
-	Problems.append(Problem)
+    Problems = []
 
-	for Recombination in Recombinations:
-		for Problem in Problems:
+    '''
+    Problem = {}
+    Problem['name'] = 'jb'
+    Problem['config'] = {'name':Problem['name'],'dimension':50}
+    Problem['generate_pop'] = ProblemRepresentation.generateJBPopulation
+    Problem['fitness'] = ProblemRepresentation.standardFitnessJB
+    Problems.append(Problem)
+    '''
 
-			Config = Problem['config']
-			Config['numb_generations'] = 20
-			Config['pop_size'] = 20
-			Config['runs'] = 3
-			Config['tournament_size'] = int(Config['pop_size']/5)
-			Config['sel_parents'] = 'tournament'
-			Config['sel_survivors'] = 'elitism'
-			Config['elite_percent'] = 0.1
-			Config['generate_pop'] = Problem['generate_pop']
-			Config['fitness_func'] = Problem['fitness']
+    Problem = {}
+    Problem['name'] = 'tsp'
+    TSPCoord = ProblemRepresentation.readTSPCoord('Data/TSP/wi29.tsp')
+    TSPDetails = ProblemRepresentation.parseTSPCoord(TSPCoord)
+    Problem['config'] = {'name': Problem['name'], 'TSPDetails': TSPDetails, 'cromo_size': len(TSPDetails),
+                         'optimization': Algorithm.Minimization}
+    Problem['generate_pop'] = ProblemRepresentation.generateTSPPopulation
+    Problem['fitness'] = ProblemRepresentation.standardFitnessTSP
+    Problems.append(Problem)
 
-			Config['prob_cross'] = 0.9
-			Config['prob_mut'] = 0.1
-			Config['recombination'] = Recombination['operator']
-			Config['mutation'] = IntegerOperators.mut_insert
+    for Recombination in Recombinations:
+        for Problem in Problems:
+            Config = Problem['config']
+            Config['numb_generations'] = 20
+            Config['pop_size'] = 20
+            Config['runs'] = 3
+            Config['tournament_size'] = int(Config['pop_size'] / 5)
+            Config['sel_parents'] = 'tournament'
+            Config['sel_survivors'] = 'elitism'
+            Config['elite_percent'] = 0.1
+            Config['generate_pop'] = Problem['generate_pop']
+            Config['fitness_func'] = Problem['fitness']
 
-			Plots = {}
-			Plots['pop_diversity'] = True
+            Config['prob_cross'] = 0.9
+            Config['prob_mut'] = 0.1
+            Config['recombination'] = Recombination['operator']
+            Config['mutation'] = IntegerOperators.mut_insert
 
-			print('Problem: ' + str(Problem['name']))
-			oAlgorithm = Algorithm(Config, Plots)
-			oAlgorithm.run()
-		
-			_func = max if oAlgorithm._reverseOrder() else min
-			BestOfAll = "%.2f" % _func(oAlgorithm.StatBestByRun) 
-			print('## Best of All: ' + BestOfAll)
+            Plots = {}
+            Plots['pop_diversity'] = True
 
-			#Visual.plotBestAverage(oAlgorithm.StatBestByRun,oAlgorithm.StatAvgBestByRun)
-			print('')
+            print('Problem: ' + str(Problem['name']))
+            oAlgorithm = Algorithm(Config, Plots)
+            oAlgorithm.run()
+
+            _func = max if oAlgorithm._reverseOrder() else min
+            BestOfAll = "%.2f" % _func(oAlgorithm.StatBestByRun)
+            print('## Best of All: ' + BestOfAll)
+
+            # Visual.plotBestAverage(oAlgorithm.StatBestByRun,oAlgorithm.StatAvgBestByRun)
+            print('')
